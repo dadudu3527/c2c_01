0f2be00481e5a76b347cc33f4ddbe88e1ba22c3b
gaoshuai <1597436048@qq.com>
2019-07-27 22:37:09 +0800

    将电商项目添加到版本库

---
+++ ttshop/buyer_apps/cart/models.py
@@ -0,0 +1,4 @@
+from django.db import models
+
+# Create your models here.
+# 我们的cart使用redis存储
---
+++ ttshop/buyer_apps/user/urls.py
@@ -0,0 +1,13 @@
+
+from django.conf.urls import url
+
+from django.contrib.auth.decorators import login_required
+from buyer_apps.user.views import *
+
+app_name = 'buyer_apps.user'
+
+urlpatterns = [
+
+
+
+]
